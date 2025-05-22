ff5390a135eefbc1530f199ef83bb27e1864ea54
ga94rib_ghaith <ga94rib@mytum.de>
2025-05-22 16:02:08 +0200

    Add missing config

--- server/src/monitoring/src/main/java/com/nicheexplorer/monitoring/MonitoringApplication.java
+++ server/src/monitoring/src/main/java/com/nicheexplorer/monitoring/MonitoringApplication.java
@@ -1,11 +1,11 @@
-package com.nicheexplorer.growthcalculation;
+package com.nicheexplorer.monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GrowthCalculationApplication {
+public class MonitoringApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GrowthCalculationApplication.class, args);
+        SpringApplication.run(MonitoringApplication.class, args);
     }
 } 
