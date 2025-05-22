71feeced152825f413e6216aeecad4d76672334b
ga94rib_ghaith <ga94rib@mytum.de>
2025-05-22 12:17:46 +0200

    added docker configuration

---
+++ server/src/content-ingestion/src/main/java/com/nicheexplorer/contentingestion/ContentIngestionApplication.java
@@ -0,0 +1,11 @@
+package com.nicheexplorer.contentingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentIngestionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentIngestionApplication.class, args);
+    }
+} 
---
+++ server/src/trend-dashboard/src/main/java/com/nicheexplorer/trenddashboard/TrendDashboardApplication.java
@@ -0,0 +1,11 @@
+package com.nicheexplorer.trenddashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrendDashboardApplication.class, args);
+    }
+} 
