134bcb8bed7d6dd349b5d70a3582c38488ebbbd6
Ghaith Allah Chebil <32078979+ghaithallahchebil@users.noreply.github.com>
2025-05-30 00:41:44 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region                  = "us-east-1"
-  access_key              = var.aws_access_key
-  secret_key              = var.aws_secret_key
+  access_key              = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key              = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 }
 
 resource "aws_instance" "example" {
