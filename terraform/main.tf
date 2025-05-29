1d2ab084207ee1efe459d0180f56171dd3d82d55
Ghaith Allah Chebil <32078979+ghaithallahchebil@users.noreply.github.com>
2025-05-30 01:04:31 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-  region                  = "us-east-1"
-  
-
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
 
   tags = {
